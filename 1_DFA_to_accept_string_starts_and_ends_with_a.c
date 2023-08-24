ab3484fa9bb128b247c651ca19e86a614a384568
Bandi Murali Chowdary <97547174+bandimuralichowdary@users.noreply.github.com>
2023-08-24 09:47:18 +0530

    Create 1_DFA_to_accept_string_starts_and_ends_with_a.c

---
+++ 1_DFA_to_accept_string_starts_and_ends_with_a.c
@@ -0,0 +1,37 @@
+// 1-DFA for strings that start with a and end with a
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char s[100];
+	int i,flag;
+	int l;
+	printf("Enter the string to check:");
+	scanf("%s",&s);
+	l=strlen(s);
+	flag=1;
+	for(i=0;i<l;i++)
+	{
+		if(s[i]!='a' && s[i]!='b')
+		{
+			flag=0;
+		}
+	}
+	if(flag!=1)
+	{
+		printf("String is not valid\n");
+	}
+	if(flag==1)
+	{
+		if(s[0]=='a' && s[l-1]=='a')
+		{
+			printf("Accepted\n");
+		}
+		else
+		{
+			printf("Not Accepted\n");
+		}
+	}
+	return 0;
+}
